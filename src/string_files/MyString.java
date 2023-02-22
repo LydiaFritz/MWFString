23412dda7e4f0a471cfe7580a37875a4d92e5f20
Lydia <lfritz4@asu.edu>
2023-02-22 08:27:06 -0800

    initial commit

---
+++ src/string_files/MyString.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package string_files;
+
+/**
+ * @author a4432_asu
+ *
+ */
+public class MyString {
+	private char [] arr;
+	private int curr_len;
+	
+	public MyString(){
+		arr = null;
+		curr_len = 0;
+	}
+}
---
+++ src/tester/Driver.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package tester;
+
+import string_files.MyString;
+
+/**
+ * @author a4432_asu
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MyString str = new MyString();
+
+	}
+
+}
