bc672e3a22ed0739d14f32b67776c71c1b4d3a55
Lydia <lfritz4@asu.edu>
2023-02-27 06:46:32 -0800

    added all constructors

--- src/string_files/MyString.java
+++ src/string_files/MyString.java
@@ -15,4 +15,25 @@ public class MyString {
 		arr = null;
 		curr_len = 0;
 	}
+	
+	public MyString(String str) {
+		//make the array
+		arr = new char[str.length()];
+		//put the characters from str into the array
+		for(int i = 0; i < str.length(); i++) {
+			arr[i] = str.charAt(i);
+		}
+		curr_len = str.length();
+	}
+	
+	public MyString(MyString str) {
+		//make the new array
+		arr = new char[str.curr_len];
+		//copy the chars
+		for(int i = 0; i < str.curr_len; i++) {
+			arr[i] = str.arr[i];
+		}
+		//update the length
+		curr_len = str.curr_len;
+	} 
 }
